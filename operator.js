6f331b22076ba46e9d1ee331c9320e69ce626d59
shivendra <shivendrakeshari581@gmail.com>
2024-06-15 23:55:38 +0530

    complete

--- operator.js
+++ operator.js
@@ -24,7 +24,46 @@ console.log(x)
 console.log(Math.floor(7/2));  //3 output  
 console.log( parseInt(7/2) ); //3 output
 
-console.log("hello");
+
+// Logicla operator 
+let v = true && true;   // true
+let y = true && false;  // false
+let z = false && false; // false
+
+console.log(v); // Output: true
+console.log(y); // Output: false
+console.log(z); // Output: false
+
+// Example with variables
+let a = 5;
+let b = 10;
+console.log(a > 0 && b > 0); // Output: true
+console.log(a > 0 && b < 0); // Output: false
+
+
+
+
+let m = true || true;   // true
+let n = true || false;  // true
+let o = false || false; // false
+
+console.log(m); // Output: true
+console.log(n); // Output: true
+console.log(o); // Output: false
+
+// Example with variables
+let q = 5;
+let e = -10;
+console.log(q > 0 || e > 0); // Output: true
+console.log(q < 0 || e < 0); // Output: true
+
+// 0 is treated as false 
+// 1 is treated as true 
+console.log(4 && 0);
+console.log(4 || 0);
+console.log(0 || 4);
+
+
 
 
 
