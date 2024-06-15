f852a649235c332d0ed5e4eb74399aba4aa6bcbf
shivendra <shivendrakeshari581@gmail.com>
2024-06-15 20:45:10 +0530

    add types of value in js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Numbers
+console.log(22);    // Integer number
+console.log(2.4);   // Floating point number
+console.log(-3);    // Negative integer
+console.log(0);
+console.log(-0);
+
+// Boolean
+console.log(false); // Boolean value false
+console.log(true);  // Boolean value true
+
+// null and undefined
+let y ;
+console.log(y); // Output: null
+// console.log(null);       // A null value
+// console.log(undefined);  // Undefined value (variable has not been assigned a value)
+let p = null;
+console.log(p); // Output: null
+
+// Strings
+console.log("hello");   // Double-quoted string
+console.log('hello');   // Single-quoted string
+console.log(`hello`);   // Template literal string
