f96189f70330de85c68c3454459a180a7cb30914
shivendra <shivendrakeshari581@gmail.com>
2024-06-15 23:11:40 +0530

    fllor division

---
+++ operator.js
@@ -0,0 +1,30 @@
+console.log(2+3);
+console.log(3-2);
+console.log(10/2);
+console.log(10%2);
+console.log(3*2);
+
+
+// Assigment operator
+console.log(`Assignement operator`)
+let x =12;
+x +=2;
+console.log(x)
+x -=2;
+console.log(x)
+x *=4
+console.log(x)
+x /=2;
+console.log(x)
+x %=3
+console.log(x)
+
+// if i want the floor value of decimal then i have two way...
+
+console.log(Math.floor(7/2));  //3 output  
+console.log( parseInt(7/2) ); //3 output
+
+console.log("hello");
+
+
+
