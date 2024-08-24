ea495554a773b97ead1a8e2158d2f03880c6cd1f
shivendra <shivendrakeshari581@gmail.com>
2024-08-24 22:37:30 +0530

    made changes

---
+++ oops.js
@@ -0,0 +1,27 @@
+// classes in javascrpt
+// class is a blueprint for creating objects with pre-defined properties and methods
+
+// example:-
+class product {
+    name;
+    price;
+    discount;
+    desc;
+    // no need of let var and const 
+
+    dusplay(){
+        // no need of functionm keyword 
+
+    }
+    buy(){
+        // 
+
+    }
+    whishlist(){
+        console.log("My wishlist is empty");
+
+    }   
+
+}
+
+// syntax to create an oject 
